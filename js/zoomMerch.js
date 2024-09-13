df5131de4962739fe70f5321fde4e9603fece3bd
slyvenegas <venegas@gmail.com>
2024-09-13 10:38:52 -0500

    ajustes MERCH

--- js/zoomMerch.js
+++ js/zoomMerch.js
@@ -1,25 +1,16 @@
-document.querySelectorAll('.slide img').forEach((img) => {
-    const magnifier = document.createElement('div');
-    magnifier.classList.add('magnifier');
-    img.parentElement.appendChild(magnifier);
-  
-    img.addEventListener('mousemove', (e) => {
-      const rect = img.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-      const bgX = (x / rect.width) * 100;
-      const bgY = (y / rect.height) * 100;
-  
-      magnifier.style.display = 'block';
-      magnifier.style.left = `${x - magnifier.offsetWidth / 2}px`;
-      magnifier.style.top = `${y - magnifier.offsetHeight / 2}px`;
-      magnifier.style.backgroundImage = `url(${img.src})`;
-      magnifier.style.backgroundPosition = `${bgX}% ${bgY}%`;
-      magnifier.style.backgroundSize = `${img.width * 2}px ${img.height * 2}px`; /* Ajusta el tamaño de zoom */
-    });
-  
-    img.addEventListener('mouseleave', () => {
-      magnifier.style.display = 'none';
-    });
+// script.js
+document.querySelectorAll('.slider-container .slide img').forEach((img) => {
+  img.addEventListener('mousemove', function (e) {
+    const rect = img.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const xPercent = (x / rect.width) * 100;
+    const yPercent = (y / rect.height) * 100;
+
+    img.style.transformOrigin = `${xPercent}% ${yPercent}%`;
   });
-  
+
+  img.addEventListener('mouseleave', function () {
+    img.style.transformOrigin = 'center';
+  });
+});
